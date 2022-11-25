72e78c7a3ea99036fe722557d7c002861512da82
Parathan Thiyagalingam <parathanlive123@gmail.com>
2022-11-25 09:29:33 +0530

    changes

--- src/userAuth.js
+++ src/userAuth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://3.109.49.42/api',
+    baseURL: 'http://3.111.166.230/api',
 })
 
 export default axiosInstance;
